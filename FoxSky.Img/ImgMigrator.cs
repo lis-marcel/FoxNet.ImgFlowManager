1447ed5a37504d7d3de55a7ad09d74f927f2fb7f
lis-marcel <lis.marcys@gmail.com>
2022-04-05 11:21:57 +0200

    IMG-2: added method for extracting date from exif

--- FoxSky.Img/ImgMigrator.cs
+++ FoxSky.Img/ImgMigrator.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MetadataExtractor;
+using MetadataExtractor.Formats.Exif;
 
 namespace FoxSky.Img
 {
@@ -15,6 +17,7 @@ namespace FoxSky.Img
         public string? Prefix { get; set; }
         #endregion
 
+        
         #region Public methods
         public void GetFilesFromSrc(string srcPath)
         {
@@ -22,7 +25,7 @@ namespace FoxSky.Img
             
             if (File.Exists(strPath)) 
                 ProcessFile(strPath);
-            else if (Directory.Exists(strPath)) 
+            else if (System.IO.Directory.Exists(strPath)) 
                 ProcessDirectory(strPath);
             else 
                 Console.WriteLine("{0} is not a valid file or directory.", srcPath);
@@ -42,7 +45,7 @@ namespace FoxSky.Img
         public void ProcessDirectory(string targetDirectory)
         {
             //Process found files
-            var files = Directory.EnumerateFiles(targetDirectory, "*.jpg");
+            var files = System.IO.Directory.EnumerateFiles(targetDirectory, "*.jpg");
             
             foreach (var fileName in files)
             {
@@ -52,22 +55,27 @@ namespace FoxSky.Img
         #endregion
 
         #region Private methods
-        private string PrepareDstDir(DateTime photoDate)
+        private string PrepareDstDir(DateTime? photoDate)
         {
-            var dstRoot = Path.Combine(DstRootPath, photoDate.Year.ToString());
+            var dstRoot = photoDate.HasValue ?
+                Path.Combine(DstRootPath, photoDate.Value.Year.ToString()) :
+                DstRootPath;
 
-            if (!Directory.Exists(dstRoot))
+            if (!System.IO.Directory.Exists(dstRoot))
             {
-                Directory.CreateDirectory(dstRoot);
+                System.IO.Directory.CreateDirectory(dstRoot);
             }
 
             return dstRoot;
         }
 
-        private string PrepareNewFileName(string srcFileName, string dstPath, DateTime photoDate)
+        private string PrepareNewFileName(string srcFileName, string dstPath, DateTime? photoDate)
         {
-            var fileName = photoDate.ToString("yyyyMMdd_hhmmss");
-            var extension = Path.GetExtension(fileName).Trim();
+            var fileName = photoDate.HasValue ?
+                photoDate.Value.ToString("yyyyMMdd_HHmmss") :
+                Path.GetFileNameWithoutExtension(srcFileName);
+
+            var extension = Path.GetExtension(srcFileName).Trim();
             var newFileName = Path.Combine(dstPath, fileName) + extension;
             int i = 1;
 
@@ -93,14 +101,31 @@ namespace FoxSky.Img
 
         private bool SameBinaryContent(string fileName1, string fileName2)
         {
-            return false; // TODO: compare binaries
+            byte[] file1 = File.ReadAllBytes(fileName1);
+            byte[] file2 = File.ReadAllBytes(fileName2);
+
+            if (file1.Length == file2.Length)
+            {
+                for (int i = 0; i < file1.Length; i++)
+                {
+                    if (file1[i] != file2[i]) return false;
+                }
+
+                return true;
+            }
+
+            return false;
         }
         
-        private DateTime ExtractPhotoDateFromExif(string fileName)
+        private DateTime? ExtractPhotoDateFromExif(string fileName)
         {
-            var exif = File.GetCreationTimeUtc(fileName);
+            DateTime dateTime;
 
-            return exif;
+            return ImageMetadataReader.ReadMetadata(fileName)?
+                .OfType<ExifSubIfdDirectory>()?
+                .FirstOrDefault()?
+                .TryGetDateTime(ExifDirectoryBase.TagDateTimeOriginal, out dateTime) == true ? dateTime : null;
+
         }
 
         #endregion
