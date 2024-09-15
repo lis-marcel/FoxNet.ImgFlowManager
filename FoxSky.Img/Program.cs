6f7be106e585a67da26fe3cdb5f4eb3e00533a46
marcel <lis.marcys@gmail.com>
2024-09-16 00:20:05 +0200

    added cache for geolocation

--- FoxSky.Img/Program.cs
+++ FoxSky.Img/Program.cs
@@ -7,14 +7,14 @@ class Program
     {
         var resSuccess = false;
 
-        if (args.Length < 3)
+        if (args.Length < 4)
         {
             ImgMigrator.LogError("Invalid params.");
         }
         else
         {
-            var imgMigrator = new ImgMigrator() { PicsOwnerSurname = args[0], SrcPath = args[1], DstRootPath = args[2] };
-            resSuccess = imgMigrator.ProcessImages();
+            var imgMigrator = new ImgMigrator() { PicsOwnerSurname = args[0], SrcPath = args[1], DstRootPath = args[2], Distance = args[3] };
+            resSuccess = Task.Run(async () => await imgMigrator.ProcessImages()).GetAwaiter().GetResult();
         }
 
         Environment.ExitCode = resSuccess ? 0 : 1;
